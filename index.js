735529b6e4215d75a6ba50f093ac9105a9f69818
Antriksh Mahajan <antrikshamahajan5@gmail.com>
2021-08-22 15:32:28 +0530

    data fetched

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { request, response } = require("express");
 
 const express = require("express");
 
+const Database  = require("./database");
 //initialization
 const ourAPP =express();
 
@@ -9,4 +10,8 @@ ourAPP.get("/",(request,response)=>{
 
     response.json({message:"Request Served"});
 });
+ourAPP.get("/book",(req,res)=>{
+    return res.json({books:Database.Book}); 
+});
+
 ourAPP.listen(4000,()=>console.log("server is running"));
